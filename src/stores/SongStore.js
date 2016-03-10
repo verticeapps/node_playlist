825b557760f55c0e56cc44ef24d2381694d791f8
Daniel Pagharion <dpaghari@gmail.com>
2016-03-10 01:31:29 -0800

    Installed flux and am using it's dispatcher for our dispatcher.js to register and listen to events, Applied to SongStore store

--- src/stores/SongStore.js
+++ src/stores/SongStore.js
@@ -1,5 +1,5 @@
 import { EventEmitter } from "events";
-
+import dispatcher from "../dispatcher";
 class SongStore extends EventEmitter {
   constructor(){
     super();
@@ -30,7 +30,18 @@ class SongStore extends EventEmitter {
   getAll() {
     return this.songs;
   }
+
+  handleActions(action) {
+    switch(action.type){
+      case "CREATE_SONG":
+        console.log('sup');
+      break;
+    }
+    //console.log("SongStore received an action", action);
+  }
 }
 
 const songStore = new SongStore();
+dispatcher.register(songStore.handleActions.bind(songStore));
+//window.dispatcher = dispatcher;
 export default songStore;
